f659f7261c028ff5d3825160e1804fba8f4eba11
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-29 08:46:07 -0400

    all tests passing

--- api/secrets/index.js
+++ api/secrets/index.js
@@ -1,11 +1,12 @@
 /**
-  Fix this module so other modules can require JWT_SECRET into them.
-  Use the || operator to fall back to the string "shh" to handle the situation
-  where the process.env does not have JWT_SECRET.
+ Fix this module so other modules can require JWT_SECRET into them.
+ Use the || operator to fall back to the string "shh" to handle the situation
+ where the process.env does not have JWT_SECRET.
 
-  If no fallback is provided, TESTS WON'T WORK and other
-  developers cloning this repo won't be able to run the project as is.
+ If no fallback is provided, TESTS WON'T WORK and other
+ developers cloning this repo won't be able to run the project as is.
  */
-module.exports = {
+ const JWT_SECRET = process.env.JWT_SECRET || 'ssh'
 
-}
+
+ module.exports = {JWT_SECRET}
